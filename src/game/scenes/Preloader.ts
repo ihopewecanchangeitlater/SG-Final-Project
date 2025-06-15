2077a608966c8cf365e56ce2851178dd57b4dca9
Kostas Thomson <kostasthomson12@gmail.com>
2025-06-15 15:20:08 +0300

    gh pages v3

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -6,7 +6,7 @@ export class Preloader extends Scene {
 	}
 
 	preload() {
-		this.load.setPath("/assets");
+		this.load.setPath(`${import.meta.env.BASE_URL}/assets`);
 		this.load.image("logo", "logo.png");
 	}
 
