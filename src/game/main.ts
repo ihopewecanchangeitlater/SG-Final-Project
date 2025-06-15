9e9e080a380713dd35359d87ee57f7506f801717
KrKaterina <katerinakrotka01@gmail.com>
2025-06-15 12:25:04 +0300

    merged

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,15 +1,11 @@
 import { Boot } from './Boot';
 import { GameOver } from './GameOver';
-import { Game as MainGame } from './Game';
+import { Game as MainGame } from './stathis/Game';
 import { MainMenu } from './MainMenu';
 import { AUTO, Game } from 'phaser';
-<<<<<<< HEAD:src/game/main.ts
-import { Preloader } from './scenes/Preloader';
-import { DragToCategory } from  './scenes/DragToCategory';
-=======
+
+
 import { Preloader } from './Preloader';
-import NameThePictureScene from './scenes/NameThePictureScene';
->>>>>>> stathis:src/game/scenes/stathis/main.ts
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
@@ -21,15 +17,10 @@ const config: Phaser.Types.Core.GameConfig = {
     backgroundColor: '#028af8',
     scene: [
         Boot,
-        NameThePictureScene,    
         Preloader,
-
-        DragToCategory,
-
         MainMenu,
         MainGame,
         GameOver
-        
     ]
 };
 
